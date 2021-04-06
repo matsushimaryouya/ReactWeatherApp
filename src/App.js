61703f4d4cc723bc7839365ed5054e74e5e1aa7d
Ryouya matsushima <blue.conjrer@gmail.com>
2021-04-06 11:59:37 +0900

    add web animation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import './App.css';
-import GifAnim from './woman_anim.gif';
 import React from 'react';
-import EffectAnim from './rain.mp4'
+import WomanLongAnim from './long.mp4'
+import WomanHalfAnim from './half.mp4'
+import SunAnim from './sun.mp4'
+import CloudsAnim from './clouds.mp4'
+import RainAnim from './rain.mp4'
 // axiosで通信を行う
 import axios from 'axios';
 import { useForm } from "react-hook-form";
 import ReactPlayer from "react-player";
+// Drawer
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import MenuOpenIcon from '@material-ui/icons/MenuOpen';
 
-
-// API通信Getのやり方（axiosを使用）
 function App() {
 
   // apiの情報をstate管理
-  const [statusName, setName] = React.useState('loading');
-  const [statusWeather, setWeather] = React.useState('loading');
+  const [statusName, setName] = React.useState('');
+  const [statusWeather, setWeather] = React.useState('');
   const [city, setCity] = React.useState('Tokyo');
   const [temperature, setTemp] = React.useState();
   const [maxTemperature, setMaxTemp] = React.useState();
   const [minTemperature, setMinTemp] = React.useState();
+  const [closeNav, setCloseNav] = React.useState(false);
   const topUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
   const bottomUrl = ',jp&appid=ab772c4cd38076eff2acf4701e560be6';
-
   // react-hook-form
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => {
     setCity(data.exampleRequired);
   }
-
 
   // React.useEffect(() => { ... }, []);を入れることでaxiosの繰り返し処理を止めることができる
   React.useEffect(() => {
@@ -43,21 +48,25 @@ function App() {
         setTemp(temp);
         setMaxTemp(maxTemp);
         setMinTemp(minTemp);
-    
+        
         // catchでエラー時の挙動を定義する
       }).catch(err => {
         console.log('err:', err);
       });
-    // 第二引数に[]を入れるとaxiosの繰り返し処理がなくなる
-  });
+      // 第二引数に[]を入れるとaxiosの繰り返し処理がなくなる
+    });
 
+    const navClick = () => {
+      setCloseNav(!closeNav);
+    }
+    const navState = closeNav?'closeNav': 'openNav';
   return (
     <>
+    {/* woman animation */}
     <div className="App">
-      <img className="humanGif" src={GifAnim}></img>
       <ReactPlayer
-      className="effectAnim"
-      url={EffectAnim}
+      className="womanAnim"
+      url={(maxTemperature >= 25)?WomanHalfAnim:WomanLongAnim}
       controls={false}
       loop
       config={{ file: { attributes: {
@@ -65,12 +74,53 @@ function App() {
         muted: true
       }}}}
       />
+      {/* clear effects */}
+      <div className={(statusWeather == 'Clear')?'fadeInEffect':' '}>
+      <ReactPlayer
+      className={'EffectAnim Clear'}
+      url={SunAnim}
+      controls={false}
+      loop
+      config={{ file: { attributes: {
+        autoPlay: true,
+        muted: true
+      }}}}
+      />
+      </div>
 
+      {/* clouds effects */}
+      <div className={(statusWeather == 'Clouds')?'fadeInEffect':' '}>       
+      <ReactPlayer
+      className={'EffectAnim Clouds'}
+      url={CloudsAnim}
+      controls={false}
+      loop
+      config={{ file: { attributes: {
+        autoPlay: true,
+        muted: true
+      }}}}
+      />
+      </div>
 
-      <div className="weatherNav">
-        <br></br>
-        <br></br>
+      {/* rain effects */}
+      <div className={(statusWeather == 'Rain')?'fadeInEffect':' '}>
+      <ReactPlayer
+      className={'EffectAnim Rain'}
+      url={RainAnim}
+      controls={false}
+      loop
+      config={{ file: { attributes: {
+        autoPlay: true,
+        muted: true
+      }}}}
+      />
+      </div>
 
+      <div className={"weatherNav" + ' ' + navState}>
+      <Button onClick={navClick}><MenuOpenIcon /></Button>
+        <p>日本の都市を検索できます。
+        <br></br>アルファベットでご入力ください。
+        </p>
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* include validation with required or other standard HTML validation rules */}
           <input className="inputTxt" placeholder="例)Tokyo, Osaka etc.." {...register("exampleRequired", { required: true })} />
@@ -79,11 +129,11 @@ function App() {
           {errors.exampleRequired && <p>フォームに入力されていません</p>}
         </form>
 
-        <h1>City:{statusName}</h1>
-        <h1>Weather:{statusWeather}</h1>
-        <h1>temp:{temperature}</h1>
-        <h1>MaxTemp:{maxTemperature}</h1>
-        <h1>MinTemp:{minTemperature}</h1>
+        <h1 className={statusName}>場所:{statusName}</h1>
+        <h1>天気:{statusWeather}</h1>
+        <h1>現在の気温:{temperature}℃</h1>
+        <h1>最高気温:{maxTemperature}℃</h1>
+        <h1>最低気温:{minTemperature}℃</h1>
       </div>
 
       </div>
